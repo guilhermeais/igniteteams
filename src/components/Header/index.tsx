15784879f2ed90c5f8d12281ef61d68c5223aa18
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-11-15 22:40:34 -0300

    feat: create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,18 @@
-import { Container, Logo } from './style'
 import logoImage from '@assets/logo.png'
+import { BackButton, BackIcon, Container, Logo } from './style'
 
-export function Header() {
+export type HeaderProps = {
+  showBackButton?: boolean
+  onBack?: () => void
+}
+export function Header({ showBackButton, onBack = () => void 0 }: HeaderProps) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton onPress={onBack}>
+          <BackIcon />
+        </BackButton>
+      )}
       <Logo source={logoImage} />
     </Container>
   )
