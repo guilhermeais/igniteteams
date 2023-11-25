3495111a36e4453a7723511f933eb02a80168e37
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-11-25 11:45:42 -0300

    feat(Groups): create state to groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,16 +2,40 @@ import { Header } from '@components/Header'
 import { Container } from './styles'
 import { Highlight } from '@components/Highlight'
 import { GroupCard } from '@components/GroupCard'
+import { useEffect, useState } from 'react'
+import { FlatList } from 'react-native'
+
+type Group = {
+  title: string
+}
 
 export function Groups() {
+  const [groups, setGroups] = useState<Group[]>([])
+
+  useEffect(
+    () =>
+      setGroups([
+        {
+          title: 'Turma A',
+        },
+        {
+          title: 'Turma B',
+        },
+      ]),
+    []
+  )
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Nome da turma" />
-      <GroupCard title="Nome da turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item, i) => `${item.title}-${i}`}
+        renderItem={({ item }) => <GroupCard title={item.title} />}
+      />
     </Container>
   )
 }
