a1f964b73223b103c93d66a14dc127953a1c458c
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2023-11-25 11:25:40 -0300

    feat(GroupCard): create group card

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,13 +1,17 @@
 import { Header } from '@components/Header'
 import { Container } from './styles'
 import { Highlight } from '@components/Highlight'
+import { GroupCard } from '@components/GroupCard'
 
 export function Groups() {
   return (
     <Container>
       <Header />
 
-      <Highlight title='Turmas' subtitle='jogue com a sua turma' />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+
+      <GroupCard title="Nome da turma" />
+      <GroupCard title="Nome da turma" />
     </Container>
   )
 }
